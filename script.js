ef0202a5a228156674e094d6ef0e1d9aa8644172
OniHony <90738273+OniHony@users.noreply.github.com>
2021-09-29 11:50:39 -0400

    JavaIntro

---
+++ script.js
@@ -0,0 +1,28 @@
+let world = "Hello World"
+console.log (world)
+console.log(23 + 97)
+let max = 57 
+let actual = (max - 13)
+console.log(actual)
+let percentage = (actual / max)
+console.log(percentage)
+
+
+	const a = 1;
+	const b = 1;
+
+	let result = (a + b);
+
+	
+	console.log(result)
+
+	number = Number(prompt("enter a number"));
+
+	function numberChecker() {
+		if(number >= 10) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
